cc179846c83712f69895e5a750705d09090edf19
614854822 <mohamed.haaji@bt.com>
2024-12-10 15:29:50 +0000

    Update Index

--- index.php
+++ index.php
@@ -2,11 +2,7 @@
 error_reporting(E_ALL & ~E_NOTICE);
 ini_set('display_errors', 'On');
 
-$page = $_GET['p'];
-
-if(!$page){
-    $page = "home";
-}
+$page = isset($_GET['p']) ? $_GET['p'] : "home";
 
 require_once(__DIR__.'/includes/header.php');
 require_once(__DIR__.'/pages/'.$page.'.php');
